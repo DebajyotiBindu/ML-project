9cb24b63901dcc97ac9eefa192831569e9172159
Debajyoti <debajyotibindu1@gmail.com>
2025-08-30 19:53:52 +0530

    Did Hyperparameter tuning with GridSearchCV

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 import dill
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -17,11 +18,16 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
         for i in range(len(models)):
             model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
+
+            gs_model=GridSearchCV(model,param,cv=3,n_jobs=-1,verbose=False)
+            gs_model.fit(X_train,y_train)
+            model.set_params(**gs_model.best_params_)
             #Train the model
             model.fit(X_train,y_train)
             #Predicting the model
